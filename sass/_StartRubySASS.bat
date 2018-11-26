8a55afcb5fbfde4f5e6d374563ccc47b27fd86eb
Tobias Hattinger <tobias.hattinger@gmx.at>
2018-11-26 19:15:52 +0100

    Initial commit
    + Added base directory structure
    + Added gitignore
    + Added htaccess

---
+++ sass/_StartRubySASS.bat
@@ -0,0 +1,18 @@
+@echo off
+color a
+#Path to the Ruby-CMD executable
+echo C:\Windows\System32\cmd.exe /E:ON /K C:\Ruby25-x64\bin\setrbvars.cmd
+cls
+echo         ===========================================
+echo         *             RUBY - MONITOR              *
+echo         *         LIFE SASS-CSS WATCHDOG          *
+echo         * COMPATIBLE WITH *.SCSS and *.SASS FILES *
+echo         *                                         *
+echo         *           CLOSE WITH CTRL + C           * 
+echo         ===========================================
+echo.
+S:
+cd /D S:\XAMPP\htdocs\KroKoDeal
+sass --watch sass:css
+
+pause >nul
